9b8c7c44ce145b076151d005299363feaa50d4c9
SNnT23 <79445089+SNnT23@users.noreply.github.com>
2023-08-18 19:42:21 +0630

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = "16950780"
+API_HASH     = "b2cc1795a8537e3e848fa8d0cb1fd503"
+BOT_TOKEN    = "5312364349:AAFNwBLC6wGKhX6XpGpeSSeeeQJoj1NbDdc"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
+DATABASE_URI = "mongodb+srv://gclonee123:<password>@cluster0.xt3juw6.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001960004039
+ADMIN        = 16950780
 CHANNEL      = "@GreyMatter_Bots"
